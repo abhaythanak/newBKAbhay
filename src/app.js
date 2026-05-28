e4c1c08581508ace539a2e04a5e2a146136eedb4
abhaydev-ada <frontend.dev1@adalovelacetechnologies.com>
2026-05-28 11:39:44 +0530

    app.use order of righting the code matters in routing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,25 @@ const express = require("express");
 const app = express()
 const PORT = 3001 
 
-app.use("/bat",(req,res)=>{
+app.use("/",(req,res)=>{
     res.send("Hello ADA")
 })
+app.use("/abhay",(req,res)=>{
+    res.send("abhay route")
+})
+app.use("/soumen",(req,res)=>{
+    res.send("soumen route")
+})
+app.use("/rahul",(req,res)=>{
+    res.send("rahul route")
+})
+app.use("/dipankar",(req,res)=>{
+    res.send("dipankar route")
+})
+app.use("/biswajit",(req,res)=>{
+    res.send("biswajit route")
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
