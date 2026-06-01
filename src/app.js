ba014e01e384887c7da5ad47e454e1972bfdc435
abhaydev-ada <frontend.dev1@adalovelacetechnologies.com>
2026-06-01 15:17:46 +0530

    fix: replace test route with global error handling middleware in app.js

--- src/app.js
+++ src/app.js
@@ -2,24 +2,22 @@ const express = require("express");
 const app = express()
 const PORT = 3001 
 const {userAuth} = require('./auth')
+app.use("/",(err,req,res)=>{
+if (err){
+    res.status(500).send("something went wrong")
+}
+})
 
 app.get("/user",userAuth,(req,res)=>{
     res.send({name:"abhay",age:21,gender:"male"})
 })
 // this will match all http method like get,post,put,delete,patch,options,head,trace
-app.use("/test",[(req,res,next)=>{
-   // res.send("Hello ADA")
-   console.log("hello")
-   next();
-},(req,res,next)=>{
-    console.log("next1")
-    next();
-},(req,res,next)=>{
-    console.log("next2")
-    res.send("hello")
-    next()
-}])
 
+app.use("/",(err,req,res)=>{
+    if(err){
+        res.status(500).send("something went wrong")
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
