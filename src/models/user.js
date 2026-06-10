9408ac1888c542e992a86a1abf19f27f7ac7061f
abhaydev-ada <frontend.dev1@adalovelacetechnologies.com>
2026-06-10 16:25:35 +0530

    feat: integrate validator for password security and add extended fields to user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,50 +1,60 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
 
-const userModel = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 50
-    },
-    lastName: {
-        type: String,
-    },
-    emailId: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase:true,
-        trim:true,
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: String,
-        min: 18,
-        
-    },
-    gender: {
-        type: String,
-        validate(value){
-            if (!["male","female","others"].includes(value)){
-                throw new Error("Gender data is not valid")
+// use validator for schema base validation
+const userModel = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 3,
+            maxLength: 50
+        },
+        lastName: {
+            type: String,
+        },
+        emailId: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            validate(value) {
+                if (!validator.isStrongPassword(value)) {
+                    throw new Error("Enter Strong Password:" + value)
+                }
             }
+        },
+        age: {
+            type: String,
+            min: 18,
+
+        },
+        gender: {
+            type: String,
+            validate(value) {
+                if (!["male", "female", "others"].includes(value)) {
+                    throw new Error("Gender data is not valid")
+                }
+            }
+        },
+        photoUrl: {
+            type: String,
+            default: "https://thumbs.dreamstime.com/b/human-brain-gray-background-brainstorming-concept-human-brain-floating-dark-background-134264032.jpg?w=768"
+        },
+        about: {
+            type: String,
+            default: "this is the default about the user"
+        },
+        skills: {
+            type: [String],
         }
     },
-    photoUrl: {
-        type: String,
-        default:"https://thumbs.dreamstime.com/b/human-brain-gray-background-brainstorming-concept-human-brain-floating-dark-background-134264032.jpg?w=768"
-    },
-    about: {
-        type: String,
-        default: "this is the default about the user"
-    },
-    skills: {
-        type: [String],
-    }
-})
+    { timestamps: true }
+)
 
 module.exports = mongoose.model("User", userModel) 
